1eaf968a3be9f912780a49b800dbc1efe326daa0
vikram <vikramvy@gmail.com>
2020-08-29 20:42:10 -0500

    Initial commit

---
+++ src/main/java/com/example/order/OrdermgmtApplication.java
@@ -0,0 +1,13 @@
+package com.example.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermgmtApplication.class, args);
+	}
+
+}
